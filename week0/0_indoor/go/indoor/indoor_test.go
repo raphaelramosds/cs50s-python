d7fa8dc3bb7c78f98110856f43abd0357fa9d4c9
Raphael Ramos <raphael.opensource@gmail.com>
2026-05-03 08:46:25 -0300

    Add additional test cases for Indoor function

--- week0/0_indoor/go/indoor/indoor_test.go
+++ week0/0_indoor/go/indoor/indoor_test.go
@@ -8,3 +8,17 @@ func TestHELLO(t *testing.T) {
 		t.Errorf("%s should be lowercase", txt)
 	}
 }
+
+func TestTHIS_IS_CS50(t *testing.T) {
+	txt := Indoor("THIS IS CS50")
+	if txt != "this is cs50" {
+		t.Errorf("%s should be lowercase", txt)
+	}
+}
+
+func Test50(t *testing.T) {
+	txt := Indoor("50")
+	if txt != "50" {
+		t.Errorf("%s should be lowercase", txt)
+	}
+}
